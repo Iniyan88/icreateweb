32526418f5168a0fb1a37c37a6e6ce55d8722b75
iniyan88 <biniyan2004@gmail.com>
2024-02-16 20:54:23 +0530

    aiml

--- frontend/myapp/src/_main/materials/AiMl.jsx
+++ frontend/myapp/src/_main/materials/AiMl.jsx
@@ -8,11 +8,6 @@ const AiMl = () => {
     { id: 3, title: "Card 3" },
     { id: 4, title: "Card 4" },
     { id: 5, title: "Card 5" },
-    { id: 6, title: "Card 6" },
-    { id: 7, title: "Card 7" },
-    { id: 8, title: "Card 8" },
-    { id: 9, title: "Card 9" },
-    { id: 10, title: "Card 10" },
   ];
 
   const [searchTerm, setSearchTerm] = useState("");

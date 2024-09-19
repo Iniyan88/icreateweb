699ea72c627c959ebb5cfd889a6367c9f4102ee5
INIYAN B <127330110+Iniyan88@users.noreply.github.com>
2024-09-19 22:33:35 +0530

    Update appwriteConfig.js

--- frontend/myapp/src/appwrite/appwriteConfig.js
+++ frontend/myapp/src/appwrite/appwriteConfig.js
@@ -1,15 +1,15 @@
 import { Account, Client, Storage, Databases, Avatars } from "appwrite";
 export const appwriteConfig = {
-  databaseId: "65bf176fc0223f74a827",
+  databaseId: "",
   storageId: "",
-  userCollectionId: "65bf1b92d330c22fc161",
-  contactCollectionId: "65c30d2ef3198a2680a2",
-  starCollectionId: "65caeda18b783031ca6a",
+  userCollectionId: "",
+  contactCollectionId: "",
+  starCollectionId: "",
 };
 const client = new Client();
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("65be0215528b11edd6a7");
+  .setProject("");
 export const account = new Account(client);
 export const database = new Databases(client);
 export const storage = new Storage(client);
